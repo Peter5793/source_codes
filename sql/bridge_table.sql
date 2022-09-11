ec99a53a8f708f178eacf03f8551b4831ff7d4c6
Peter Lugalia <p.lugalia@launchpad.alx.app>
2022-09-11 15:06:22 +0200

    many to many relationship

---
+++ sql/bridge_table.sql
@@ -0,0 +1,14 @@
+SELECT 
+    --fs.SalesOrderNumber AS InvoiceNumber,
+    --fs.SalesOrderLineNumber AS InvoiceLineNumber,
+    dsr.SalesReasonReasonType AS SalesReason,
+    sum(fs.SalesAmount) AS SalesAmount
+FROM FactInternetSales AS fs 
+    INNER JOIN FactInternetSalesReason as fsr 
+    ON fs.SalesOrderNumber = fsr.SalesOrderNumber AND fs.SalesOrderLineNumber = fsr.SalesOrderLineNumber
+    INNER JOIN DimSalesReason AS dsr 
+    -- Bridging table
+    ON fsr.SalesReasonKey = dsr.SalesReasonKey
+--WHERE fs.SalesOrderNumber = N'SO51178'
+
+GROUP BY dsr.SalesReasonReasonType
